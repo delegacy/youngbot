ee13213e8a48853ffd11219682c5664b3cd679bd
YoungTae Seok <delegacy@gmail.com>
2019-05-07 23:51:26 +0900

    Use Ansible for deployment

--- deploy/ecs-deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-# update an AWS ECS service with the new image
-aws ecs update-service --cluster "cluster-young-bot-server" \
-                       --service "service-young-bot-server" \
-                       --task-definition "td-young-bot-server" \
-                       --force-new-deployment
