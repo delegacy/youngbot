c193c647d012f8dc4a4fe181e208167864bc0c70
YoungTae Seok <delegacy@gmail.com>
2019-09-15 23:54:57 +0900

    Deploy to Heroku (#24)

--- ansible-playbook/ansible-playbook.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-export ANSIBLE_LOG_PATH=$(dirname $0)/ansible-playbook.log.$(date +%Y-%m-%d-%H%M%S)
-echo "[$(date +%Y-%m-%dT%H:%M:%S%Z)] ANSIBLE PLAYBOOK: $@" >> $ANSIBLE_LOG_PATH
-exec ansible-playbook --diff "$@"
