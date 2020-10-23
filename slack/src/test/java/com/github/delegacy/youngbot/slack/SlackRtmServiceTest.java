1666f64400da0d11109530e2b6374e87ebd9825b
YoungTae Seok <delegacy@gmail.com>
2020-10-23 21:12:24 +0900

    Support threaded messages for Slack RTM (#45)

--- slack/src/test/java/com/github/delegacy/youngbot/slack/SlackRtmServiceTest.java
+++ slack/src/test/java/com/github/delegacy/youngbot/slack/SlackRtmServiceTest.java
@@ -2,6 +2,7 @@ package com.github.delegacy.youngbot.slack;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.awaitility.Awaitility.await;
+import static org.junit.jupiter.api.Assertions.assertSame;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.ArgumentMatchers.anyString;
@@ -18,11 +19,13 @@ import java.util.concurrent.TimeUnit;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import com.github.delegacy.youngbot.message.MessageResponse;
 import com.github.delegacy.youngbot.message.MessageService;
+import com.github.delegacy.youngbot.slack.SlackRtmService.MessageWithThreadTs;
 import com.slack.api.Slack;
 import com.slack.api.bolt.App;
 import com.slack.api.bolt.AppConfig;
@@ -32,6 +35,7 @@ import com.slack.api.rtm.RTMClient;
 import com.slack.api.rtm.RTMCloseHandler;
 import com.slack.api.rtm.RTMErrorHandler;
 import com.slack.api.rtm.RTMMessageHandler;
+import com.slack.api.util.json.GsonFactory;
 
 import reactor.core.publisher.Flux;
 
@@ -70,16 +74,16 @@ class SlackRtmServiceTest {
     @Test
     void testHelloEventHandler_whenPingTaskIsNull(@Mock ScheduledFuture future) throws Exception {
         final SlackRtmService.HelloEventHandler helloEventHandler = slackRtmService.new HelloEventHandler();
-        when(scheduledExecutorService.scheduleAtFixedRate(
+        when(scheduledExecutorService.scheduleWithFixedDelay(
                 any(SlackRtmService.PingTask.class), anyLong(), anyLong(), any(TimeUnit.class)))
                 .thenReturn(future);
 
         helloEventHandler.handle(new HelloEvent());
 
         assertThat(slackRtmService.sessionClosed).isFalse();
-        verify(scheduledExecutorService).scheduleAtFixedRate(
+        verify(scheduledExecutorService).scheduleWithFixedDelay(
                 any(SlackRtmService.PingTask.class), anyLong(), anyLong(), any(TimeUnit.class));
-        assertThat(slackRtmService.pingTaskFuture == future).isTrue();
+        assertSame(future, slackRtmService.pingTaskFuture);
     }
 
     @SuppressWarnings("rawtypes")
@@ -91,9 +95,9 @@ class SlackRtmServiceTest {
         helloEventHandler.handle(new HelloEvent());
 
         assertThat(slackRtmService.sessionClosed).isFalse();
-        verify(scheduledExecutorService, never()).scheduleAtFixedRate(
+        verify(scheduledExecutorService, never()).scheduleWithFixedDelay(
                 any(SlackRtmService.PingTask.class), anyLong(), anyLong(), any(TimeUnit.class));
-        assertThat(slackRtmService.pingTaskFuture == future).isTrue();
+        assertSame(future, slackRtmService.pingTaskFuture);
     }
 
     @Test
@@ -119,17 +123,28 @@ class SlackRtmServiceTest {
 
     @Test
     void testMessageEventHandler() throws Exception {
-        when(messageService.process(any())).thenReturn(
-                Flux.just(MessageResponse.of(new SlackMessageRequest("ping", "channel"), "PONG")));
-
-        final SlackRtmService.MessageEventHandler messageHandler = slackRtmService.new MessageEventHandler();
-
         final MessageEvent event = new MessageEvent();
         event.setChannel("channel");
         event.setText("ping");
+        event.setThreadTs("thread");
+
+        when(messageService.process(any())).thenReturn(
+                Flux.just(MessageResponse.of(SlackMessageRequest.of(event), "PONG")));
+
+        final SlackRtmService.MessageEventHandler messageHandler = slackRtmService.new MessageEventHandler();
 
         messageHandler.handle(event);
 
-        await().atMost(Duration.ofSeconds(1)).untilAsserted(() -> verify(rtmClient).sendMessage(anyString()));
+        await().atMost(Duration.ofSeconds(1)).untilAsserted(() -> {
+            final ArgumentCaptor<String> textCaptor = ArgumentCaptor.forClass(String.class);
+            verify(rtmClient).sendMessage(textCaptor.capture());
+
+            final MessageWithThreadTs msg = GsonFactory.createSnakeCase()
+                                                       .fromJson(textCaptor.getValue(),
+                                                                 MessageWithThreadTs.class);
+            assertThat(msg.getText()).isEqualTo("PONG");
+            assertThat(msg.getChannel()).isEqualTo("channel");
+            assertThat(msg.getThreadTs()).isEqualTo("thread");
+        });
     }
 }
